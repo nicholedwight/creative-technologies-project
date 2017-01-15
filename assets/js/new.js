f039e2350b95c3af4bf1cc6746a185e67ec91e85
Nichole Dwight <nicholedwight@gmail.com>
2017-01-15 20:05:44 +0000

    adding tonejs, works onclick of mesh, need to vary the sound by dragging mesh around

--- assets/js/new.js
+++ assets/js/new.js
@@ -14,6 +14,8 @@
     var controls;
     var objects = [];
     var control;
+    var raycaster;
+		var mouse;
 
     function init() {
         renderer = new THREE.WebGLRenderer({
@@ -79,7 +81,6 @@
       scene.add( control );
   		scene.add(mesh);
       objects.push(mesh);
-
     }
 
     function render() {
@@ -102,7 +103,92 @@
         init();
     }
 
-    /** Window load event kicks off execution */
+    // Tone js code
+
+    var count = 0;
+    var pad = "0000000000";
+    var previousBinarySet = pad.split('');
+    var notes = ["D4", "E4", "F4", "G4", "A5", "C5", "D5", "E5", "F5", "G5"];
+    var times = [1.7, 1.5, 1.3, 1.1, .9, .7, .5, .5, .5, .5];
+
+    function toBinary(number){
+      return (number >>> 0).toString(2);
+    }
+
+    function padNumber(numberString) {
+      return pad.substring(0, pad.length - numberString.length) + numberString;
+    }
+
+    function play() {
+
+      var synth = new Tone.PolySynth(16, Tone.SimpleSynth).set({
+        "volume" : -8,
+        "oscillator" : {
+          "type" : "triangle1",
+          frequency: 90,
+          detune: 0,
+          phase: 0
+        },
+        "envelope" : {
+          "attack" :  0.025,
+          "decay" :  0.35,
+          "sustain" :  0.18,
+          "release" :  0.2,
+        },
+      }).toMaster();
+
+      Tone.Transport.bpm.value = 50;
+
+      Tone.Transport.scheduleRepeat(function(time) {
+
+        var number = Math.floor(count);
+        count = count + 1;
+        var numberString = padNumber(toBinary(number));
+
+        var binaryArray = numberString.split('');
+
+        for( var i = 0; i < binaryArray.length; i++ ) {
+          if( binaryArray[i] === "1" && previousBinarySet[i] !== binaryArray[i]) {
+            synth.triggerAttackRelease(notes[i], times[i], time);
+          }
+        }
+
+        previousBinarySet = binaryArray;
+
+        if( count > 1023 ) {
+          count = 0;
+        }
+      }, "32i");
+
+      Tone.Transport.start();
+    }
+
+    /* End Tone.js code */
+
+    function onDocumentMouseDown( event ) {
+			event.preventDefault();
+			mouse.x = ( event.clientX / renderer.domElement.clientWidth ) * 2 - 1;
+			mouse.y = - ( event.clientY / renderer.domElement.clientHeight ) * 2 + 1;
+
+			raycaster.setFromCamera( mouse, camera );
+
+			var intersects = raycaster.intersectObjects( objects );
+
+			if ( intersects.length > 0 ) {
+        console.log('clicked');
+        play();
+		  }
+    }
+
+    function stopMusic() {
+      Tone.Transport.stop();
+    }
+
+    raycaster = new THREE.Raycaster();
+    mouse = new THREE.Vector2();
+    var stopButton = document.getElementById('stop');
+    stopButton.addEventListener('click', stopMusic);
+    document.addEventListener( 'mousedown', onDocumentMouseDown, false );
     window.addEventListener('load', onWindowLoaded, false);
     window.addEventListener('resize', onWindowResize, false);
     window.addEventListener( 'keydown', function ( event ) {
